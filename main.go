f79e65888351dc2fc46a4a319a8067458e39ab51
xh <11675084@qq.com>
2021-03-12 16:29:38 +0800

    cookie

--- main.go
+++ main.go
@@ -3,33 +3,16 @@ package main
 import (
 	"embed"
 	"fmt"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 
 	"gindemo/controller"
 )
 
-//go:embed static
+//go:embed static/**
 var static embed.FS
 
 func main() {
-
-	// dsn := "xx:FMKmpFkKD2GMkcjc@tcp(192.168.137.3:3306)/xx?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-	// 	NamingStrategy: schema.NamingStrategy{
-	// 		TablePrefix:   "heng_", // 表名前缀，`User` 的表名应该是 `t_users`
-	// 		SingularTable: true,    // 使用单数表名，启用该选项，此时，`User` 的表名应该是 `t_user`
-	// 	},
-	// })
-
-	// // db, err := gorm.Open("mysql", "root:xiangheng@/meddit?charset=utf8&parseTime=True&loc=Local")
-
-	// if err != nil {
-	// 	log.Println("数据库连接错误", err)
-	// 	return
-	// }
-	// db.AutoMigrate(&model.User{}) //自动迁移仅仅会创建表，缺少列和索引
 
 	// router:= gin.New()
 	router := gin.Default()
@@ -38,22 +21,38 @@ func main() {
 	// router.Static("/static", "./static") //静态文件
 
 	router.POST("/login", controller.Login)
+	router.POST("/upload", controller.Upload)
 
 	router.GET("/reg", controller.Reg)
 	router.GET("/checklogin", controller.CheckLogin)
 
-	router.POST("/upload", controller.Upload)
+	router.GET("/cookie", func(c *gin.Context) {
+		fmt.Printf("%#v \n", c.Request)
+		cookie, err := c.Cookie("gin_cookie")
+
+		if err != nil {
+			cookie = "NotSet"
+			c.SetCookie("gin_cookie", "test", 3600, "/", c.Request.Host, false, true)
+		}
+		// c.SetCookie("gin_cookie", "test", 3600, "/", c.Request.Host, false, true)
+
+		fmt.Printf("Cookie value: %s , %s\n", cookie, err)
+	})
 
 	/**
 	生成路径
 	/public/static/
 	*/
-	router.StaticFS("/public", http.FS(static)) //静态文件 嵌入程序中
+	// router.StaticFS("/public", http.FS(static)) //静态文件 嵌入程序中
+	// router.StaticFS("/public", static.ReadDir("static")) //静态文件 嵌入程序中
+	// router.StaticFS("/", http.FS(static))
 
-	fmt.Println("地址： http://127.0.0.1:8080")
+	router.StaticFile("/", "./static/index.html")
+	router.Static("/static", "./static")
 
+	IP_PORT := ":10080"
+	fmt.Println("地址： http://127.0.0.1" + IP_PORT)
 	// util.GenRsaKey(2048)
-
-	router.Run()
+	router.Run(IP_PORT)
 
 }
